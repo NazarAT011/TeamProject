d56218a043aabce4f8a7d9ae2a737610b717a86f
Nazar Kosar <zik2009@ukr.net>
2015-09-09 22:28:10 +0300

    init

---
+++ src/main/java/LITS/ChupilKreminKosar/App.java
@@ -0,0 +1,13 @@
+package LITS.ChupilKreminKosar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
